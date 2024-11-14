612211c9b0852cddd28117d693001dc99a81baa8
Connor Frank <conjfrnk@gmail.com>
2024-11-13 23:04:16 -0500

    Initial Commit

---
+++ BioPulse/BioPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioPulseApp.swift
+//  BioPulse
+//
+//  Created by Connor Frank on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BioPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BioPulseTests/BioPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  BioPulseTests.swift
+//  BioPulseTests
+//
+//  Created by Connor Frank on 11/13/24.
+//
+
+import Testing
+@testable import BioPulse
+
+struct BioPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
